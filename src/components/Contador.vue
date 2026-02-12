b92aca2f2116e4cec61aef53ccc1bc5978304272
Tomii313 <tominob993@gmail.com>
2026-02-12 19:32:32 -0300

    Componentes de Contador

---
+++ src/components/Contador.vue
@@ -0,0 +1,29 @@
+
+<template>
+  
+  <h1>Hola bro</h1>
+  <p>{{ contador }}</p>
+  <button @click="incrementar">Incrementar</button>
+  <button @click="decrementar">Decrementar</button>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+const contador = ref(0);
+
+const incrementar = () => {
+  contador.value++;
+}
+
+const decrementar = () => {
+  contador.value -= 1;
+}
+</script>
+
+
+<style scoped>
+p{
+  color:red;
+}
+
+</style>
